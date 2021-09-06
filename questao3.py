96672600524e2a8b5fd5820761a3cfe9abc12833
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-06 15:44:48 -0300

    Questão 3

---
+++ questao3.py
@@ -0,0 +1,8 @@
+N1 = int(input("Insira o primeiro número(inteiro): "))
+N2 = int(input("Insira o segundo número(inteiro):  "))
+N3 = int(input("Insira o terceiro número(inteiro):  "))
+N4 = int(input("Insira o quarto número(inteiro):  "))
+N5 = int(input("Insira o quinto número(inteiro):  "))
+
+lista = [N1,N2,N3,N4,N5]
+print("A soma dos números da lista foram: ",sum(lista))
