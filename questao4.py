2f37866d7de39fa967f652df15686f96beb9c986
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-06 16:37:19 -0300

    Questão 4 finalizada

--- questao4.py
+++ questao4.py
@@ -1,4 +1,5 @@
-FRUTAS = ["banana","mogarano",'laranja','melaca','jeca']
+#Escreva um programa para copiar uma lista em outra.
 
+FRUTAS = ["banana","mogarano",'laranja','melaca','jeca']
 FRUTAS2PONTOZERO =  FRUTAS.copy()
 print("Cópia da lista frutas: ",FRUTAS2PONTOZERO)
