5ffb971021635df4e8126b493214fc5c75e88f55
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-06 16:37:33 -0300

    Questão 6 finalizada

--- questao6.py
+++ questao6.py
@@ -1,3 +1,4 @@
+#Escreve um programa que verifque a posição exata de um elemento da lista.
 FRUTAS = []
 FRUTAS = ["banana","morango",'laranja','abacate','jecaa']
 procurar = input("Quer pesquisar a posição de qual palavra?")
