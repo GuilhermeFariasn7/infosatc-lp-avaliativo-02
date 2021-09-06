77637efd6cf8a5858e84f4ac2d66a5db2b70715d
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-06 16:37:13 -0300

    Questão 3 finalizada

--- questao3.py
+++ questao3.py
@@ -1,6 +1,7 @@
+#Escreva um programa para somar todos os itens em uma lista
 N1 = int(input("Insira o primeiro número(inteiro): "))
 N2 = int(input("Insira o segundo número(inteiro):  "))
-N3 = int(input("Insira o terceiro número(inteiro):  "))
+N3 = int(input("Insira o terceiro número(inteiro): "))
 N4 = int(input("Insira o quarto número(inteiro):  "))
 N5 = int(input("Insira o quinto número(inteiro):  "))
 
